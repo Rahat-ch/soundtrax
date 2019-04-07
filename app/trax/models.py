f943c149e6cfa988287accc4776a2aaf9ca2b87f
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-04-07 16:28:04 -0400

    migrated likes

--- app/trax/models.py
+++ app/trax/models.py
@@ -8,3 +8,7 @@ class Track(models.Model):
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(get_user_model(), null= True, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE),
+    track = models.ForeignKey('trax.Track', related_name='likes', on_delete=models.CASCADE)
