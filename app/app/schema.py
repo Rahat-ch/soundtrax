ac4fb3afe8a491cadc4e1386c566a2b2f736ed48
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-04-07 14:06:07 -0400

    backend authentication and added user to trax data

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,11 +1,14 @@
 import graphene
 import trax.schema
 import users.schema
+import graphql_jwt
 
 class Query(users.schema.Query, trax.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, trax.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
